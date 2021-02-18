760e3cf72d097325d5cf16fff7b5e479adc9afe3
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2021-02-18 21:18:04 +0000

    made quiz look better

--- assets/js/newquiz.js
+++ assets/js/newquiz.js
@@ -1,178 +1,195 @@
-// select all elements
-const start = document.getElementById("start");
+// const declaration to select elements.
+const startbtn = document.getElementById("startbtn");
 const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const qImg = document.getElementById("qImg");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
+const questiontext = document.getElementById("questiontext");
+const quizimg = document.getElementById("quizimg");
+const optionA = document.getElementById("A");
+const optionB = document.getElementById("B");
+const optionC = document.getElementById("C");
 const counter = document.getElementById("counter");
-const timeGauge = document.getElementById("timeGauge");
-const progress = document.getElementById("progress");
-const scoreDiv = document.getElementById("scoreContainer");
+const timeLeft = document.getElementById("timeLeft");
+const progressCircles = document.getElementById("progressCircles");
+const scoreContainer = document.getElementById("scoreContainer");
+const resetButton = document.getElementById("resetButton");
+const infoButton = document.getElementById("infoButton");
+const quizDiv = document.getElementById("quiz");
+const containerDiv = document.getElementById("container");
 
-// create our questions
+
+
+
+// Questions array
 let questions = [
   {
-    question: "How much food waste produced globally goes to waste?",
+    questiontext: "How much food waste produced globally goes to waste?",
     imgSrc: "assets/images/quiz-1.jpg",
-    choiceA: "33%",
-    choiceB: "73%",
-    choiceC: "10%",
+    optionA: "33%",
+    optionB: "73%",
+    optionC: "10%",
     correct: "A",
   },
   {
-    question:
+    questiontext:
       "What is the economical value of the food wasted every year in the world?",
     imgSrc: "assets/images/quiz-2.jpg",
-    choiceA: "$10 Billion",
-    choiceB: "$1 Billion",
-    choiceC: "$1 Trillion",
+    optionA: "$10 Billion",
+    optionB: "$1 Billion",
+    optionC: "$1 Trillion",
     correct: "C",
   },
   {
-    question:
+    questiontext:
       "What percentage of the total waster supply is used to grow food never eaten?",
     imgSrc: "assets/images/quiz-3.jpg",
-    choiceA: "10th%",
-    choiceB: "20th",
-    choiceC: "3rd",
+    optionA: "10th%",
+    optionB: "20th",
+    optionC: "3rd",
     correct: "C",
   },
   {
-    question:
+    questiontext:
       "How much food would be needed to feed the nearly one billion hungry people in the world?",
     imgSrc: "assets/images/quiz-4.jpg",
-    choiceA: "1/4",
-    choiceB: "2x",
-    choiceC: "The Annual Total ",
+    optionA: "1/4",
+    optionB: "2x",
+    optionC: "The Annual Total ",
     correct: "C",
   },
   {
-    question:
+    questiontext:
       "What can we do to be able to feed the extra 2.3 billion people that will leave on Earth by 2050?",
     imgSrc: "assets/images/quiz-5.jpg",
-    choiceA: "Reduce Food Waste to 0",
-    choiceB: "Increase food production by 300%",
-    choiceC: "Increase food production 100% and reduce food wasted by 20%.",
+    optionA: "Reduce Food Waste to 0",
+    optionB: "Increase food production by 300%",
+    optionC: "Increase food production 100% and reduce food wasted by 20%.",
     correct: "C",
   },
 ];
 
+
+
 const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
+let actualQuestion = 0;
 let count = 0;
-const questionTime = 10; // 10s
-const gaugeWidth = 150; // 150px
-const gaugeUnit = gaugeWidth / questionTime;
-let TIMER;
+const maxTime = 10;
+const gaugeWidth = 150; 
+const gaugeUnit = gaugeWidth / maxTime;
+let stopWatch;
 let score = 0;
 
-// render a question
-function renderQuestion() {
-  let q = questions[runningQuestion];
 
-  question.innerHTML = "<p>" + q.question + "</p>";
-  qImg.innerHTML = "<img src=" + q.imgSrc + ">";
-  choiceA.innerHTML = q.choiceA;
-  choiceB.innerHTML = q.choiceB;
-  choiceC.innerHTML = q.choiceC;
+
+//Start Quiz
+startbtn.addEventListener("click", initializeQuiz);
+
+//Change Bg to White when the Quick starts
+startbtn.addEventListener("click", function () {
+  $('#container').css("background","#fff");
+});
+
+function renderQuestion() {
+  let q = questions[actualQuestion];
+  questiontext.innerHTML = "<p>" + q.questiontext + "</p>";
+  questionImage.innerHTML = "<img src=" + q.imgSrc + ">";
+  optionA.innerHTML = q.optionA;
+  optionB.innerHTML = q.optionB;
+  optionC.innerHTML = q.optionC;
 }
 
-start.addEventListener("click", startQuiz);
 
-// start quiz
-function startQuiz() {
-  start.style.display = "none";
+function initializeQuiz() {
+  startbtn.style.display = "none";
   renderQuestion();
   quiz.style.display = "block";
   renderProgress();
   renderCounter();
-  TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
+  stopWatch = setInterval(renderCounter, 1000); // 1000ms = 1s
 }
 
-// render progress
+// Render progress
 function renderProgress() {
   for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
-    progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+    progressCircles.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
   }
 }
 
-// counter render
+//Show quiz progress
 
 function renderCounter() {
-  if (count <= questionTime) {
+  if (count <= maxTime) {
     counter.innerHTML = count;
-    timeGauge.style.width = count * gaugeUnit + "px";
+    timeLeft.style.width = count * gaugeUnit + "px";
     count++;
   } else {
     count = 0;
-    // change progress color to red
-    answerIsWrong();
-    if (runningQuestion < lastQuestion) {
-      runningQuestion++;
+    if (actualQuestion < lastQuestion) {
+      actualQuestion++;
       renderQuestion();
     } else {
       // end the quiz and show the score
-      clearInterval(TIMER);
+      clearInterval(stopWatch);
       scoreRender();
     }
   }
 }
 
-// checkAnwer
+//UP TO HERE
 
-function checkAnswer(answer) {
-  if (answer == questions[runningQuestion].correct) {
-    // answer is correct
-    score++;
-    // change progress color to green
-    answerIsCorrect();
+function AddScoreBgIfAnswerCorrect(answer) {
+  if (answer == questions[actualQuestion].correct) {
+        score++;
+        document.getElementById(actualQuestion).style.backgroundColor = "#115c07";
   } else {
-    // answer is wrong
-    // change progress color to red
-    answerIsWrong();
+        document.getElementById(actualQuestion).style.backgroundColor = "#f00";
   }
   count = 0;
-  if (runningQuestion < lastQuestion) {
-    runningQuestion++;
+  if (actualQuestion < lastQuestion) {
+    actualQuestion++;
     renderQuestion();
   } else {
     // end the quiz and show the score
-    clearInterval(TIMER);
+    clearInterval(stopWatch);
     scoreRender();
   }
 }
 
-// answer is correct
-function answerIsCorrect() {
-  document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
-}
-
-// answer is Wrong
-function answerIsWrong() {
-  document.getElementById(runningQuestion).style.backgroundColor = "#f00";
-}
 
 // score render
 function scoreRender() {
-  scoreDiv.style.display = "block";
+  scoreContainer.style.display = "block";
+  quizDiv.style.display = "none";
+  resetButton.style.display = "block";
+  infoButton.style.display = "block";
 
-  // calculate the amount of question percent answered by the user
-  const scorePerCent = Math.round((100 * score) / questions.length);
 
-  // choose the image based on the scorePerCent
+// Calculate Score and display it with related image depending on how well the user did.
+  const finalScore = Math.round((100 * score) / questions.length);
+
+  // Choose the image based on the finalScore
   let img =
-    scorePerCent >= 80
+    finalScore >= 80
       ? "assets/images/emo-80.png"
-      : scorePerCent >= 60
+      : finalScore >= 60
       ? "assets/images/emo-60.png"
-      : scorePerCent >= 40
+      : finalScore >= 40
       ? "assets/images/emo-40.png"
-      : scorePerCent >= 20
+      : finalScore >= 20
       ? "assets/images/emo-20.png"
       : "assets/images/emo-20.png";
 
-  scoreDiv.innerHTML = "<img src=" + img + ">";
-  scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
+  scoreContainer.innerHTML = "<img src=" + img + ">";
+  scoreContainer.innerHTML += "<p>" + finalScore + "%</p>";
 }
+
+function reloadP() {
+     window.location.reload();
+    }
+
+  
+
+  
+  
+
+  
+
+
